424a6bfcd1326c71282f3c1311d8617f76a22b40
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2017-01-09 20:52:56 +0000

    Begin setting up http server & client code.

---
+++ Nibriboard/Nibriboard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using IotWeb.Server;
+using IotWeb.Common.Http;
+namespace Nibriboard
+{
+	public class Nibriboard
+	{
+		private HttpServer httpServer;
+
+		public readonly int Port = 31586;
+
+		public Nibriboard()
+		{
+		}
+
+		public void Setup()
+		{
+			httpServer = new HttpServer(Port);
+			httpServer.AddHttpRequestHandler(
+				"/"
+			);
+		}
+	}
+}
