394f7f4284b20ff87d1bedcf6a4cc0851db107f3
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2017-02-23 18:09:48 +0000

    Bugfix validate.sh

--- Nibriboard/ClientFiles/validate.sh
+++ Nibriboard/ClientFiles/validate.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+counter_filename=$(mktemp -p /dev/shm/ -t bash.XXXXXXXXX.tmp);
+echo 0 >${counter_filename};
 find . -name "*.js" -not -path "./node_modules/*" | while read filename;
 do
 	validate_result=$(node_modules/.bin/acorn --module --silent $filename 2>&1);
@@ -8,10 +10,17 @@ do
 	echo ${filename}: ${validate_output}
 	# TODO: Use /dev/shm here since apparently while is in a subshell, so it can't modify variables in the main program O.o
 	if ! [ ${validate_exit_code} -eq 0 ]; then
-		echo incrementing ${error_count} \($(expr ${error_count} + 1)\)
-		error_count=$(expr ${error_count} + 1);
+		error_count=$(cat ${counter_filename});
+		echo incrementing ${error_count} \($(expr ${error_count} + 1)\);
+		echo $(expr ${error_count} + 1) >${counter_filename};
 	fi
 done
 
-echo 
+error_count=$(cat ${counter_filename});
 echo Errors: $error_count
+
+if [[ ${error_count} -ne 0 ]]; then
+	exit 1;
+fi
+
+exit 0;
