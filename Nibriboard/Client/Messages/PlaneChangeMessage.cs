c4947474cdcf07f9cbb9c15a250260d970d61ef5
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2017-03-20 18:10:17 +0000

    [server] Fill out PlaneChangeMessage

--- Nibriboard/Client/Messages/PlaneChangeMessage.cs
+++ Nibriboard/Client/Messages/PlaneChangeMessage.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 namespace Nibriboard.Client.Messages
 {
+	/// <summary>
+	/// Represents a plane change requested by a nirbi client.
+	/// </summary>
 	public class PlaneChangeMessage : Message
 	{
+		/// <summary>
+		/// Whether the plane change is ok or not.
+		/// Used when the server is replying to a plane change request.
+		/// </summary>
+		public bool IsOK = false;
+		/// <summary>
+		/// Ţhe new plane name that the nibri client would like to switch to.
+		/// </summary>
 		public string NewPlaneName;
 
+		/// <summary>
+		/// Create a new plane change message.
+		/// </summary>
+		/// <param name="inNewPlaneName">The new plane name that the nibri client would like to switch to.</param>
 		public PlaneChangeMessage(string inNewPlaneName)
 		{
 			NewPlaneName = inNewPlaneName;
