a8c1b8e99c3ef968e8d9248d8adcfb66b19704f5
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2017-03-29 21:21:11 +0100

    [server] Add LinePart event message class

---
+++ Nibriboard/Client/Messages/LinePartMessage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using SBRL.Utilities;
+
+namespace Nibriboard.Client.Messages
+{
+	/// <summary>
+	/// Senyt by a client who's in the middle of drawing a line.
+	/// Contains a collection of points that should be added to the line they're drawing.
+	/// </summary>
+	public class LinePartMessage : Message
+	{
+		/// <summary>
+		/// The id of the line to add the points to.
+		/// </summary>
+		public Guid LineId;
+		/// <summary>
+		/// The points to add to the line
+		/// </summary>
+		public List<Vector2> Points;
+
+		public LinePartMessage()
+		{
+		}
+	}
+}
