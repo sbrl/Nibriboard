2929bca21200ef49c16e29a9b62849659062b0fc
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2017-06-24 20:19:57 +0100

    [server] Add missing RawChunkReference

---
+++ Nibriboard/Client/RawChunkReference.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Nibriboard.Client
+{
+	public class RawChunkReference
+	{
+		public string planeName { get; set; } = string.Empty;
+		public int x { get; set; } = 0;
+		public int y { get; set; } = 0;
+	}
+}
