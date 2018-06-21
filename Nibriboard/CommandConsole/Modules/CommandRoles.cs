d767ae4b2edba731a21d06639251f9758f539752
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2018-06-21 11:43:38 +0100

    [server] Finish refactoring commands into their own files. Next we just need to hook everything back up again....

---
+++ Nibriboard/CommandConsole/Modules/CommandRoles.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Nibriboard.RippleSpace;
+using Nibriboard.Userspace;
+using Nibriboard.Utilities;
+
+namespace Nibriboard.CommandConsole.Modules
+{
+	public class CommandRoles : ICommandModule
+	{
+		private NibriboardServer server;
+
+		public ModuleDescription Description { get; } = new ModuleDescription(
+			"roles",
+			"{{subcommand}}",
+			"Manage roles"
+		);
+
+		public CommandRoles()
+		{
+		}
+
+		public void Setup(NibriboardServer inServer)
+		{
+			server = inServer;
+		}
+
+		public async Task Handle(CommandRequest request)
+		{
+			if (request.Arguments.Length < 2)
+			{
+				await request.WriteLine("Nibriboard Server Command Console: roles");
+				await request.WriteLine("----------------------------------------");
+				await request.WriteLine(Description.ToLongString());
+				await request.WriteLine();
+				await request.WriteLine("Subcommands:");
+				await request.WriteLine("    list");
+				await request.WriteLine("        Lists all roles");
+				await request.WriteLine("    grant {role-name} {username}");
+				await request.WriteLine("        Adds a role to a user");
+				await request.WriteLine("    revoke {role-name} {username}");
+				await request.WriteLine("        Removes a role from a user");
+				return;
+			}
+
+			await CommandParser.ExecuteSubcommand(this, request.Arguments[1], request);
+		}
+
+		public async Task List(CommandRequest request)
+		{
+			await request.WriteLine(string.Join("\n", server.AccountManager.Roles.Select(
+				(RbacRole role) => role.ToString()
+			)));
+		}
+
+		public async Task Grant(CommandRequest request)
+		{
+			string roleName = (request.Arguments[2] ?? "").Trim();
+			string targetUsername = (request.Arguments[3] ?? "").Trim();
+			if (roleName.Length == 0) {
+				await request.WriteLine("Error: No role name specified!");
+				return;
+			}
+			if (targetUsername.Length == 0) {
+				await request.WriteLine("Error: No username specified!");
+				return;
+			}
+
+			User user = server.AccountManager.GetByName(targetUsername);
+			RbacRole roleToGrant = server.AccountManager.ResolveRole(roleName);
+			if (user == null) {
+				await request.WriteLine($"Error: No user with the the name {targetUsername} could be found.");
+				return;
+			}
+			if (roleToGrant == null) {
+				await request.WriteLine($"Error: No role with the the name {roleName} could be found.");
+				return;
+			}
+			if (user.HasRole(roleToGrant)) {
+				await request.WriteLine($"Error: {targetUsername} already has the role {roleToGrant.Name}.");
+				return;
+			}
+
+			user.Roles.Add(roleToGrant);
+			await server.SaveUserData();
+
+			await request.WriteLine($"Role {roleToGrant.Name} added to {user.Username} successfully.");
+		}
+
+		public async Task Revoke(CommandRequest request)
+		{
+			string roleName = (request.Arguments[2] ?? "").Trim();
+			string recievingUsername = (request.Arguments[3] ?? "").Trim();
+			if (roleName.Length == 0) {
+				await request.WriteLine("Error: No role name specified!");
+				return;
+			}
+			if (recievingUsername.Length == 0) {
+				await request.WriteLine("Error: No username specified!");
+				return;
+			}
+
+			User user = server.AccountManager.GetByName(recievingUsername);
+			RbacRole roleToGrant = server.AccountManager.ResolveRole(roleName);
+			if (user == null) {
+				await request.WriteLine($"Error: No user with the the name {recievingUsername} could be found.");
+				return;
+			}
+			if (roleToGrant == null) {
+				await request.WriteLine($"Error: No role with the the name {roleName} could be found.");
+				return;
+			}
+			if (!user.HasRole(roleToGrant)) {
+				await request.WriteLine($"Error: {recievingUsername} doesn't have the role {roleToGrant.Name}.");
+				return;
+			}
+
+			user.Roles.Remove(roleToGrant);
+			await server.SaveUserData();
+
+			await request.WriteLine($"Role {roleToGrant.Name} removed from {user.Username} successfully.");
+		}
+	}
+}
---
+++ Nibriboard/CommandConsole/Modules/CommandUsers.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Nibriboard.RippleSpace;
+using Nibriboard.Userspace;
+using Nibriboard.Utilities;
+
+namespace Nibriboard.CommandConsole.Modules
+{
+	public class CommandUsers : ICommandModule
+	{
+		private NibriboardServer server;
+
+		public ModuleDescription Description { get; } = new ModuleDescription(
+			"users",
+			"{{subcommand}}",
+			"Interact with user accounts"
+		);
+
+		public CommandUsers()
+		{
+		}
+
+		public void Setup(NibriboardServer inServer)
+		{
+			server = inServer;
+		}
+
+		public async Task Handle(CommandRequest request)
+		{
+			if (request.Arguments.Length < 2)
+			{
+				await request.WriteLine("Nibriboard Server Command Console: users");
+				await request.WriteLine("----------------------------------------");
+				await request.WriteLine(Description.ToLongString());
+				await request.WriteLine();
+				await request.WriteLine("Subcommands:");
+				await request.WriteLine("    list");
+				await request.WriteLine("        Lists all users.");
+				await request.WriteLine("    add {username} {password}");
+				await request.WriteLine("        Adds a new user");
+				await request.WriteLine("    checkpassword {username} {password}");
+				await request.WriteLine("        Checks a user's password");
+				await request.WriteLine("    setpassword {username} {password}");
+				await request.WriteLine("        Resets a user's password");
+				return;
+			}
+
+			await CommandParser.ExecuteSubcommand(this, request.Arguments[1], request);
+		}
+
+		public async Task List(CommandRequest request)
+		{
+			await request.WriteLine(
+				string.Join("\n", server.AccountManager.Users.Select(
+					(User user) => $"{user.CreationTime}\t{user.Username}\t{string.Join(", ", user.Roles.Select((RbacRole role) => role.Name))}"
+				))
+			);
+		}
+
+		public async Task Add(CommandRequest request)
+		{
+			string newUsername = (request.Arguments[2] ?? "").Trim();
+			string password = (request.Arguments[3] ?? "").Trim();
+
+			if (newUsername.Length == 0)
+			{
+				await request.WriteLine("Error: No username specified!");
+				return;
+			}
+			if (password.Length == 0)
+			{
+				await request.WriteLine("Error: No password specified!");
+				return;
+			}
+
+			server.AccountManager.AddUser(newUsername, password);
+			await server.SaveUserData();
+			await request.WriteLine($"Added user with name {newUsername} successfully.");
+		}
+
+		public async Task CheckPassword(CommandRequest request)
+		{
+			string checkUsername = (request.Arguments[2] ?? "").Trim();
+			string checkPassword = (request.Arguments[3] ?? "").Trim();
+
+			if (checkUsername.Length == 0)
+			{
+				await request.WriteLine("Error: No username specified!");
+				return;
+			}
+			if (checkPassword.Length == 0)
+			{
+				await request.WriteLine("Error: No password specified!");
+				return;
+			}
+
+			User checkUser = server.AccountManager.GetByName(checkUsername);
+			if (checkUser == null)
+			{
+				await request.WriteLine($"Error: User '{checkUsername}' was not found.");
+				return;
+			}
+			if (!checkUser.CheckPassword(checkPassword))
+			{
+				await request.WriteLine("Error: That password was incorrect.");
+				return;
+			}
+
+			await request.WriteLine("Password check ok!");
+		}
+
+		public async Task SetPassword(CommandRequest request)
+		{
+			string setPasswordUsername = (request.Arguments[2] ?? "").Trim();
+			string setPasswordPass = (request.Arguments[3] ?? "").Trim();
+
+			if (setPasswordUsername.Length == 0)
+			{
+				await request.WriteLine("Error: No username specified.");
+				return;
+			}
+			if (setPasswordPass.Length == 0)
+			{
+				await request.WriteLine("Error: No password specified.");
+			}
+
+			User setPasswordUser = server.AccountManager.GetByName(setPasswordUsername);
+			if (setPasswordUser == null)
+			{
+				await request.WriteLine($"Error: User '{setPasswordUsername}' wasn't found.");
+				return;
+			}
+
+			setPasswordUser.SetPassword(setPasswordPass);
+			await server.SaveUserData();
+			await request.WriteLine($"Updated password for {setPasswordUser.Username} successfully.");
+		}
+	}
+}
