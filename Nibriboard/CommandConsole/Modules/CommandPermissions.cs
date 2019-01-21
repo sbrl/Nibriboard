6455478a5f514d55af35409e47116c0075177420
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2019-01-21 23:24:18 +0000

    [server/commandconsole] Add 'perms set' subcommand

--- Nibriboard/CommandConsole/Modules/CommandPermissions.cs
+++ Nibriboard/CommandConsole/Modules/CommandPermissions.cs
@@ -35,10 +35,12 @@ namespace Nibriboard.CommandConsole.Modules
 				await request.WriteLine(Description.ToLongString());
 				await request.WriteLine();
 				await request.WriteLine("Subcommands:");
-				await request.WriteLine("    grant {{{{role:Creator|Member}}}} {{{{plane-name}}}} {{{{username}}}}");
-				await request.WriteLine("        Grant role on plane-name to username");
-				await request.WriteLine("    revoke {{{{role:Creator|Member}}}} {{{{plane-name}}}} {{{{username}}}}");
-				await request.WriteLine("        Revoke username's role on plane-name");
+				await request.WriteLine("    grant {{{{perm:Creator|Member}}}} {{{{plane-name}}}} {{{{username}}}}");
+				await request.WriteLine("        Grant permission on plane-name to username");
+				await request.WriteLine("    revoke {{{{perm:Creator|Member}}}} {{{{plane-name}}}} {{{{username}}}}");
+				await request.WriteLine("        Revoke username's role permission plane-name");
+				await request.WriteLine("    set {{{{perm:Creator|Member}}}} {{{{plane-name}}}} {{{{username}}}}");
+				await request.WriteLine("        Set username's perm on plane-name to a specific value, removing all other permissions");
 				return;
 			}
 
@@ -63,8 +65,7 @@ namespace Nibriboard.CommandConsole.Modules
 			string grantPlaneName = request.Arguments[3];
 			string grantUsername = request.Arguments[4];
 
-			if (grantRoleName.ToLower() != "creator" && grantRoleName.ToLower() != "member")
-			{
+			if (grantRoleName.ToLower() != "creator" && grantRoleName.ToLower() != "member") {
 				await request.WriteLine($"Error: Invalid role {grantRoleName}. Valid values: Creator, Member. Is not case-sensitive.");
 				return;
 			}
@@ -158,6 +159,46 @@ namespace Nibriboard.CommandConsole.Modules
 
 			await request.WriteLine($"done in {(DateTime.Now - timeStart).Milliseconds}ms.");
 
+		}
+
+		public async Task Set(CommandRequest request)
+		{
+			if (request.Arguments.Length < 5) {
+				await request.WriteLine("Error: No username specified!");
+				return;
+			}
+			if (request.Arguments.Length < 4) {
+				await request.WriteLine("Error: No plane name specified!");
+				return;
+			}
+			if (request.Arguments.Length < 3) {
+				await request.WriteLine("Error: No role specified!");
+				return;
+			}
+			string roleName = request.Arguments[2];
+			string planeName = request.Arguments[3];
+			string username = request.Arguments[4];
+
+			if (roleName.ToLower() != "creator" && roleName.ToLower() != "member") {
+				await request.WriteLine($"Error: Invalid role {roleName}. Valid values: Creator, Member. Is not case-sensitive.");
+				return;
+			}
+
+			Plane targetPlane = server.PlaneManager.GetByName(planeName);
+			if (targetPlane == null) {
+				await request.WriteLine($"Error: The plane with the name {planeName} could not be found.");
+				return;
+			}
+			if (server.AccountManager.GetByName(username) == null) {
+				await request.WriteLine($"Error: No user could be found with the name {username}.");
+				return;
+			}
+
+			targetPlane.Creators.Remove(username);
+			targetPlane.Members.Remove(username);
+
+			// The grant command handler should be able to handle it from here
+			await Grant(request);
 		}
 
 	}
